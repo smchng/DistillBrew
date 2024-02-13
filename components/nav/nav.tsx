c4f7913f106e499219d5b8135250d83b4ca67f38
Samantha <scminim100@gmail.com>
2024-02-12 16:47:00 -0800

    added cart nav

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -2,34 +2,62 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import { Bean } from "./nav_svg";
+import { Bean, Cart } from "./nav_svg";
 import menu from "@/public/svg/menu.svg";
+import cart from "@/public/svg/cart.svg";
 
 const Nav = () => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isBeanHovered, setBeanHovered] = useState(false);
+  const [isCartHovered, setCartHovered] = useState(false);
   return (
-    <div className="flex items-center fixed top-0 left-0 w-full p-[5vw] lg:p-[2vw] z-50">
-      <div
-        className="relative"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <Link href="/menu">
-          <Bean
-            fill={isHovered ? "#D4ADA0" : "white"}
-            beanFill={isHovered ? "white" : "#391F06"}
-            stylingClasses="z-50 absolute left-1/2 top-1/2 w-11 -translate-x-1/2 -translate-y-1/2 transform"
+    <div>
+      <div className="flex items-center fixed top-0 left-0 w-full p-[5vw] lg:p-[2vw] z-50">
+        <div
+          className="relative"
+          onMouseEnter={() => setBeanHovered(true)}
+          onMouseLeave={() => setBeanHovered(false)}
+        >
+          <Link href="/menu">
+            <Bean
+              fill={isBeanHovered ? "#391F06" : "white"}
+              iconFill={isBeanHovered ? "white" : "#391F06"}
+              stylingClasses1="z-50 absolute left-1/2 top-1/2 w-11 -translate-x-1/2 -translate-y-1/2 transform"
+              stylingClasses2="transition-colors"
+            />
+          </Link>
+          <Image
+            src={menu.src}
+            alt="Menu SVG"
+            width={menu.width}
+            height={menu.height}
+            className="animate-spin animate-spin-slow "
           />
-        </Link>
-        <Image
-          src={menu.src}
-          alt="Menu SVG"
-          width={menu.width}
-          height={menu.height}
-          className="animate-spin animate-spin-slow "
-        />
+        </div>
+        <h3 className="text-white mx-auto font-semibold">DISTILL BREW CO.</h3>
       </div>
-      <h3 className="text-white mx-auto font-semibold">DISTILL BREW CO.</h3>
+      <div className="fixed bottom-0 right-0 z-50 p-[1vw] lg:p-[2vw]">
+        <div
+          className="relative flex items-center"
+          onMouseEnter={() => setCartHovered(true)}
+          onMouseLeave={() => setCartHovered(false)}
+        >
+          <Link href="/cart">
+            <Cart
+              fill={isCartHovered ? "#391F06" : "white"}
+              iconFill={isCartHovered ? "white" : "#391F06"}
+              stylingClasses1=" z-50 absolute w-11 transform translate-x-8 -translate-y-1/2"
+              stylingClasses2="transition-colors"
+            />
+          </Link>
+          <Image
+            src={cart.src}
+            alt="Menu SVG"
+            width={cart.width}
+            height={cart.height}
+            className="animate-spin animate-spin-slow "
+          />
+        </div>
+      </div>
     </div>
   );
 };
