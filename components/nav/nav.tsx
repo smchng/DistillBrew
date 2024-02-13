45e74ff047e5a29642589a01f79092c73c49aa0d
Samantha <scminim100@gmail.com>
2024-02-13 11:50:31 -0800

    nav chagnes based on page

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -9,6 +10,14 @@ import cart from "@/public/svg/cart.svg";
 const Nav = () => {
   const [isBeanHovered, setBeanHovered] = useState(false);
   const [isCartHovered, setCartHovered] = useState(false);
+
+  const router = useRouter();
+
+  const isTargetPage =
+    router.pathname === "/" ||
+    router.pathname === "/about" ||
+    router.pathname === "/howTo";
+
   return (
     <div>
       <div className="flex items-center fixed top-0 left-0 w-full p-[5vw] lg:p-[2vw] z-50">
@@ -32,8 +41,16 @@ const Nav = () => {
             height={menu.height}
             className="animate-spin animate-spin-slow "
           />
-        </div>
-        <h3 className="text-brown mx-auto font-semibold">DISTILL BREW CO.</h3>
+        </div>{" "}
+        <Link href="/" className=" mx-auto ">
+          <h3
+            className={`font-semibold ${
+              isTargetPage ? "text-white" : "text-brown"
+            }`}
+          >
+            DISTILL BREW CO.
+          </h3>
+        </Link>
       </div>
       <div className="fixed bottom-0 right-0 z-50 p-[1vw] lg:p-[2vw]">
         <div
