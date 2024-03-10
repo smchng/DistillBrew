beb783dcfcfbfb5b3d849786572235d6a7053986
Samantha <scminim100@gmail.com>
2024-03-10 12:06:21 +0900

    citation layout

--- pages/citation.tsx
+++ pages/citation.tsx
@@ -2,18 +2,21 @@ export default function Citation() {
   return (
     <div className="p-[5vh] md:p-[15vh] flex flex-col justify-center h-screen">
       <div>
-        Orozco, Santiago. Designer. 2011.
-        <strong>Josefin Sans [Font]</strong>. Retrieved from
-        <a href="https://fonts.google.com/specimen/Josefin+Sans">
+        Orozco, Santiago. Designer. 2011. {""}
+        <strong>Josefin Sans [Font]</strong>. Retrieved from {""}
+        <a
+          className="text-sm"
+          href="https://fonts.google.com/specimen/Josefin+Sans"
+        >
           https://fonts.google.com/specimen/Josefin+Sans
         </a>
       </div>
 
       <div>
-        Chung, Alessandra <strong>Shizukana Siphon Renders</strong>
+        Chung, Alessandra. 2024. <strong>Shizukana Siphon Renders</strong>
       </div>
       <div>
-        Ileto, Alexandra <strong>Bean and Cart Icon</strong>
+        Ileto, Alexandra. 2024. <strong>Bean and Cart Icon</strong>
       </div>
     </div>
   );
